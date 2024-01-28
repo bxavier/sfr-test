9b7fd1172e4068a1b4967b9e9dffe55c6538de20
Bruno Xavier <bruno@brunoxavier.com.br>
2024-01-28 01:56:17 -0300

    button styling

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   standalone: true,
   imports: [],
   templateUrl: './button.component.html',
-  styleUrl: './button.component.scss'
+  styleUrl: './button.component.scss',
 })
 export class ButtonComponent {
+  @Input() label: string = 'FullWidth';
+  @Input() color: string = 'primary';
+  @Input() type: string = 'button';
+  @Input() disabled: boolean = false;
+  @Output() clickAction = new EventEmitter<void>();
 
+  onClick() {
+    this.clickAction.emit();
+  }
 }
---
+++ src/app/views/home/home.component.ts
@@ -0,0 +1,52 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { ButtonComponent } from '../../shared/components/button/button.component';
+
+interface Item {
+  id: number;
+  name: string;
+  documentNumber: string;
+  birthDate: string | Date;
+  registrationDate: string | Date;
+  montlyIncome: number;
+  email: string;
+}
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+  imports: [CommonModule, ButtonComponent],
+})
+export class HomeComponent {
+  items: Item[] = [
+    {
+      id: 1,
+      name: 'Bruno 1',
+      documentNumber: '123456789',
+      birthDate: '01/02/1981',
+      registrationDate: '01/01/2021',
+      montlyIncome: 1000.0,
+      email: 'bruno@brunoxavier.com.br',
+    },
+    {
+      id: 2,
+      name: 'Bruno 2',
+      documentNumber: '123456789',
+      birthDate: '01/02/1981',
+      registrationDate: '01/01/2021',
+      montlyIncome: 1000.0,
+      email: 'bruno@brunox.com.br',
+    },
+    {
+      id: 3,
+      name: 'Bruno 3',
+      documentNumber: '123456789',
+      birthDate: '01/02/1981',
+      registrationDate: '01/01/2021',
+      montlyIncome: 1000.0,
+      email: 'asd@brun.com',
+    },
+  ];
+}
